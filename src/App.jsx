4b21156f9bad0820fd13dbe8ce871e54e4fca977
Emzy-Jayyy <emediongb23@gmail.com>
2025-01-28 15:11:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,20 +24,40 @@ import DashoardLayout from './Layouts/DashoardLayout';
 import './App.css'
 
 function App() {
-  const routes = createRoutesFromElements(
-    <Route path='/' element={<RootLayout/>}>
-      <Route index element={<Login/>}/>
-      <Route path='signup' element={<Signup/>}/>
-      <Route path='forgot' element={<ForgotPassword/>}/>
-      <Route path='dashboard' element={<DashoardLayout/>}>
-        <Route index element={<Dashboard/>}/>
-        <Route path='project' element={<Projects/>}/>
-        <Route path='schedule' element={<Schedule/>}/>
-        <Route path='prospects' element={<Prospects/>}/>
-      </Route>
-    </Route>
-  )
-  const router = createBrowserRouter(routes);
+  // const routes = createRoutesFromElements(
+  //   <Route path='/' element={<RootLayout/>}>
+  //     <Route index element={<Login/>}/>
+  //     <Route path='signup' element={<Signup/>}/>
+  //     <Route path='forgot' element={<ForgotPassword/>}/>
+  //     <Route path='dashboard' element={<DashoardLayout/>}>
+  //       <Route index element={<Dashboard/>}/>
+  //       <Route path='project' element={<Projects/>}/>
+  //       <Route path='schedule' element={<Schedule/>}/>
+  //       <Route path='prospects' element={<Prospects/>}/>
+  //     </Route>
+  //   </Route>
+  // )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: "signup", element: <Signup /> },
+        { path: "forgot", element: <ForgotPassword /> },
+        {
+          path: "dashboard", 
+          element: <DashoardLayout />,
+          children: [
+            {index: true, element: <Dashboard />},
+            {path: 'project', element: <Projects />},
+            {path: 'schedule', element: <Schedule />},
+            {path: 'prospects', element: <Prospects />}
+          ] 
+        },
+      ],
+    },
+  ]);
   return (
     <RouterProvider router={router}>
 
