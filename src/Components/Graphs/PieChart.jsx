80cd0db98878a3b03b3cbde0c597ef1ec58398b6
Emzy-Jayyy <emediongb23@gmail.com>
2025-01-28 15:02:39 +0100

    Update PieChart.jsx

--- src/Components/Graphs/PieChart.jsx
+++ src/Components/Graphs/PieChart.jsx
@@ -4,16 +4,16 @@ import { Chart as ChartJS, registerables } from "chart.js";
 ChartJS.register(...registerables);
 const PieChart = () => {
   const pieData = {
-    labels: ["Strong", "Medium", "Low","Very Low"],
+    labels: ["STRONG FINANCE($1M+)", "MEDIUM ($500k-$1M)", "AVERAGE(Below $500k)","LOW (Below $200k)"],
     datasets: [
       {
         label: "Client Financial Strength",
         data: [1216, 4641, 747, 579,],
         backgroundColor: [
-          "rgba(255, 99, 132, 0.6)",
-          "rgba(54, 162, 235, 0.6)",
-          "rgba(255, 206, 86, 0.6)",
-          "rgba(75, 192, 192, 0.6)",
+          "rgba(0, 102, 14, 0.6)",
+          "rgba(0, 127, 211, 0.6)",
+          "rgba(247, 191, 52, 0.6)",
+          "rgba(214, 7, 0, 0.6)",
         ],
         hoverOffset: 4,
       },
